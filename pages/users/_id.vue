fc11b72b7eecbd96b680a21334e42b21aebe7b55
realing29 <realing29@gmail.com>
2019-12-03 22:33:39 +0500

    Урок 12

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,11 +1,24 @@
 <template lang='pug'>
-  h1 User {{$route.params.id}}
+  h1 {{user.name}}
 </template>
 <script>
+import { resolve } from 'q'
 export default {
   validate({params}){
 // проверка валидации на число
     return /^\d+$/.test(params.id)
+  },
+  asyncData({params, error}){
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(error(new Error('User not found')))
+        // resolve({
+        //   user: {
+        //     name: `Test user ${params.id}`
+        //   }
+        // })
+      }, 1500)
+    })
   }
 }
 </script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,15 +1,26 @@
 <template lang='pug'>
   section
-    h1 User page
+    h1 {{pageTitle}}
     ul
       li(v-for='user of users' :key='user')
         a(href='#' @click.prevent='goTo(user)') User {{user}}
 </template>
 <script>
 export default {
-  data() {
-    return {users: [1, 2, 3, 4, 5]}
+  asyncData() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve({
+          users: [
+            1, 2, 3, 4, 5
+          ]
+        })
+      }, 3000)
+    })
   },
+  data: () => ({
+    pageTitle: 'User page'
+  }),
   methods: {
     goTo(user){   
       this.$router.push('/users/' + user)
